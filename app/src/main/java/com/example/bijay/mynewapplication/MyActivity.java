20002c1085115d38175ad2fb232ea78b6e4434a6
vijaylaxmivl <vijaylaxmimails@gmail.com>
2014-10-17 13:54:33 +0530

    last change

--- app/src/main/java/com/example/bijay/mynewapplication/MyActivity.java
+++ app/src/main/java/com/example/bijay/mynewapplication/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MyActivity extends Activity {
@@ -13,6 +14,7 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Toast.makeText(getBaseContext(),"Hello",Toast.LENGTH_SHORT).show();
     }
 
 
@@ -25,9 +27,6 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
